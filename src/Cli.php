0ec80cd016ab0f166bcaeca8886af6846f046c09
Mikail <miklsuhanov@gmail.com>
2023-03-26 17:30:30 +1000

    cange linter src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
 function greetTheUser()
-{   
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);   
+    line("Hello, %s!", $name);
 }
