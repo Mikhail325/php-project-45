1a296cc63d81fa6bc3781ceecc0c71405699f027
Mikail <miklsuhanov@gmail.com>
2023-03-27 15:19:48 +1000

    add bin/brain-progression  src/Games/brain-progression.php

---
+++ src/Games/brain-progression.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\src\games\brainProgression;
+
+use function BrainGames\src\Engin\startGame;
+
+function brainStart()
+{
+    $startQuestion = 'What number is missing in the progression?';
+    
+    $numberOfQuestions = 3;
+    for ($i = 0; $i < $numberOfQuestions; $i++) {
+        $startNumber = rand(1, 30);
+        $lengthProgression = rand(5, 15);
+        $stepProgression = rand(1, 10);
+        $arrayProgression = [];
+        for ($j = 0; $j < $lengthProgression; $j++) {
+            $arrayProgression[] = $startNumber;
+            $startNumber += $stepProgression;
+        }
+        $indexParity = rand(0, $lengthProgression - 1);
+        $parity = $arrayProgression[$indexParity];
+        $arrayProgression[$indexParity] = '..';
+        $arrayProgression = implode(' ', $arrayProgression);
+        $arrayQuestion[$arrayProgression] = (string) $parity;
+    }
+    startGame($startQuestion, $arrayQuestion);
+}
