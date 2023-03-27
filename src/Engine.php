9abdadf8d2348d7d70df91116c7c0bd920ba833a
Mikail <miklsuhanov@gmail.com>
2023-03-27 18:36:47 +1000

    cange src/Cli.php src/Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -8,7 +8,7 @@ use function BrainGames\Cli\greetTheUser;
 
 function startGame($startQuestion, $arrayQuestion)
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('%s', $startQuestion);
