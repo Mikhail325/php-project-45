63badf3313ebda9a8819eba9098e4d663efac67a
Mikhail325 <128239777+Mikhail325@users.noreply.github.com>
2023-04-16 15:34:27 +1000

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -24,6 +24,9 @@ function brainGameStart()
 
 function isPrime(int $number)
 {
+    if ($number == 0 || $number == 1) {
+        return false;
+    }
     for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i == 0) {
             return false;
